7259751879e33e1e90881951d2e0a335e3dcdc88
vachan70 <v_achan@yahoo.com>
2020-11-22 16:45:48 +0000

    Second commit

--- src/com/uretctech/Main.java
+++ src/com/uretctech/Main.java
@@ -3,6 +3,5 @@ package com.uretctech;
 public class Main {
     public static void main(String[] args){
         System.out.println ("This is the Main class method. Hello World");
-        System.out.println ("This is the second print....");
     }
 }
