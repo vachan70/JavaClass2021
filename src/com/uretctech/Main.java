71c519b3a777ea2a4c8f1189aebafd5f24a17129
vachan70 <v_achan@yahoo.com>
2020-12-09 00:48:24 +0000

    Test2-Git Push

--- src/com/uretctech/Main.java
+++ src/com/uretctech/Main.java
@@ -3,7 +3,6 @@ package com.uretctech;
 public class Main {
     public static void main(String[] args){
         System.out.println ("This is the Main class method. Hello World");
-         System.out.println ("This is the Main class method. Hello World");
-          System.out.println ("This is the Main class method. Hello World");
+         System.out.println ("This is a test...pushing to Git");
     }
 }
