39768fbb977b5acbb516c5f3efb5eeb122dffb61
vachan70 <v_achan@yahoo.com>
2020-12-09 00:42:21 +0000

    Testing Github push

--- src/com/uretctech/Main.java
+++ src/com/uretctech/Main.java
@@ -3,5 +3,7 @@ package com.uretctech;
 public class Main {
     public static void main(String[] args){
         System.out.println ("This is the Main class method. Hello World");
+         System.out.println ("This is the Main class method. Hello World");
+          System.out.println ("This is the Main class method. Hello World");
     }
 }
